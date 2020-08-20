dabfb248f3eea0091f7bd3351ef8fab36826562f
Emanoel da Silva Almeida <emanoel2304@gmail.com>
2020-08-20 11:58:36 -0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,17 +1,16 @@
-var Date= 20
-
-
-//const startingMinutes = 01;
-//let time = startingMinutes * 60;
+const startingMinutes = 15;
+let time = startingMinutes * 60;
 
 const countdownEl = document.getElementById('countdown');
 
 setInterval(updateCountdown, 1000);
 
 function updateCountdown(){
-        const minutes =  Math.floor((Date/60) % 60);
-        let seconds = minutes % 60;                                 
-    //seconds = seconds < 15 ? '0' + seconds : seconds;    
-    countdownEl.innerHTML =`{minutes}:${seconds}`;
-date--;
+        const minutes =  Math.floor(time/60);
+        let seconds = time % 60;
+                                    
+    seconds = seconds < 15 ? '0' + seconds : seconds; 
+                                    
+    countdownEl.innerHTML  =`${minutes}:${seconds}`;
+time--;
 }
